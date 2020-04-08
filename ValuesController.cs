1c16e2c48763fb35f641409e884ba70250bc876d
Pooja-Heroor <61182859+Pooja-Heroor@users.noreply.github.com>
2020-04-08 06:03:25 -0700

    Add files via upload

---
+++ ValuesController.cs
@@ -0,0 +1,62 @@
+﻿using Insurance.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Insurance.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        InsurenceEntities db = new InsurenceEntities();
+
+      
+        [HttpGet]
+        [Route("api/getcustomer")]
+        public IEnumerable<Customer> GetCustomers()
+        {
+            return db.Customers.ToList();
+        }
+
+        [HttpPost]
+        [Route("api/insertcustomer")]
+        public IEnumerable<Customer> InsertCustomer(Customer customer)
+        {
+            db.Customers.Add(customer);
+            db.SaveChanges();
+            return db.Customers.ToList();
+
+        }
+
+        [HttpGet]
+        [Route("api/getvehicle")]
+        public List<VehicleDetail> GetVehicle()
+        {
+            return db.VehicleDetails.ToList();
+        }
+
+        [HttpPost]
+        [Route("api/vehicle")]
+        public List<VehicleDetail> VehicleInsert(VehicleDetail detail)
+        {
+            db.VehicleDetails.Add(detail);
+            db.SaveChanges();
+            return db.VehicleDetails.ToList();
+        }
+
+       /* [HttpPost]
+        [Route("api/Policy")]
+
+        public PolicyDetail PlanInsert(PolicyDetail model)
+        {
+           
+                db.PolicyDetails.Add(model);
+                db.SaveChanges();
+                return model;
+        }*/
+
+    }
+}
